15b131d92fa9c2a4db1c312ae3ca81a54f10cd5f
surojitmondal <76101081+surojitmondal@users.noreply.github.com>
2020-12-16 19:03:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.post('/signedrequest', function(req, res) {
     request(contactRequest, function(err, response, body) {
         var qr = qrcode.qrcode(4, 'L'),
             contact = JSON.parse(body).records[0],
-            text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
+            //text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
+			text = 'context::' + context + ';;';
         qr.addData(text);
         qr.make();
         var imgTag = qr.createImgTag(4);
